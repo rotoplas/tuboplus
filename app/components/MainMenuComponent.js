b34153c856538074cd057ce72d6626f7443c0825
Cristian Sierra Cardenas <crijosicar@hotmail.com>
2018-03-20 15:31:48 -0500

    remove commented line

--- app/components/MainMenuComponent.js
+++ app/components/MainMenuComponent.js
@@ -57,7 +57,7 @@ class MainMenuComponent extends Component{
 		  const value = await AsyncStorage.getItem('IS_REGISTERED');
 		  if (value !== null){
 		    // We have data!!
-				this.setState({ isRegistered: /*value === "true" ? true :*/ false });
+				this.setState({ isRegistered: value === "true" ? true :false });
 		  }
 
 		} catch (error) {
